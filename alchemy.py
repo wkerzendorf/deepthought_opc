cccbb5e79eceee5a68af82eaf51f1260f76292d5
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2018-08-22 10:29:11 +0200

    adding alchemy stuff

---
+++ alchemy.py
@@ -0,0 +1,5 @@
+def get_data_proposal(prop_id):
+    return {'comment':'test', 'ranking':4, 'similarity':3}
+
+def get_reviewer_per_proposal(ref_id):
+    return ['sdfsdf', 'sdfsd', 'sdfsdf', 'sdfsdf']
